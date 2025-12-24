91aaca31f8443b32eeb5c7a8972602a78c8efa30
Ankit Pandit <58819767+ankit1215@users.noreply.github.com>
2025-12-24 12:33:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+document.getElementById("voteForm").addEventListener("submit", function (e) {
+      e.preventDefault(); // prevent page refresh
+
+      const age = document.getElementById("age").value;
+      const name = document.getElementById("name").value;
+
+      // Validation
+      if (!age || !name) {
+        alert("Please enter valid details.");
+        return;
+      }
+
+      // Promise logic
+      const checkVotingEligibility = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (Number(age) > 18) {
+            resolve();
+          } else {
+            reject();
+          }
+        }, 4000);
+      });
+
+      // Handling promise
+      checkVotingEligibility
+        .then(() => {
+          alert(`Welcome, ${name}. You can vote.`);
+        })
+        .catch(() => {
+          alert(`Oh sorry ${name}. You aren't old enough.`);
+        });
+    });
